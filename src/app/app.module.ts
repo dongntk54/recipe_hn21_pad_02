273695d7dd056e115b74114262c590dda6d12fb4
Nguyen Tien Dong <Dongntk54@gmail.com>
2022-04-08 19:20:50 +0700

    Create general app routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { RecipesComponent } from './components/recipes/recipes.component';
+import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    RecipesComponent,
+    ShoppingListComponent
   ],
   imports: [
     BrowserModule,
